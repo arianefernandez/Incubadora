fb6cc889bb147ab0004a7bb3147efe57a4768322
arianefernandez <ariane.fernandez@opendeusto.es>
2019-02-27 16:46:51 +0100

    Added influxdb and graphana, also I have modified the sound edge.

--- emocion.py
+++ emocion.py
@@ -8,9 +8,10 @@ import argparse
 import os
 import time
 import picamera
-from mcp3008 import MCP3008
 
 class predecirEmocion:
+    
+    global emocion
     
     def fotografiar(self):
         camara = picamera.PiCamera()
@@ -19,7 +20,7 @@ class predecirEmocion:
         camara.capture('bebe.jpg')
         camara.close()
 
-    def predict(self):
+    def predict(self, ruido):
         """Make a prediction for an image."""
         project_id = 'incubadora-46527'
         compute_region = 'us-central1'
@@ -52,7 +53,7 @@ class predecirEmocion:
             params = {"score_threshold": score_threshold}
 
         response = prediction_client.predict(model_full_id, payload, params)
-        print("Resultado:")
+        #print("Resultado:")
         for result in response.payload:
             if 'Bienestar' in result.display_name:
                 bienestar=result.classification.score
@@ -63,18 +64,19 @@ class predecirEmocion:
         porcentajeB=bienestar*60/total
         porcentajeL=llanto*60/total
         
-        mcp = MCP3008(0,0)
-        ruido = mcp.leer(0)
-    
-        if(ruido > 300):
+        if(ruido > 200):
                 porcentajeL=porcentajeL+40
         else:
                 porcentajeL=porcentajeL-40
     
         if(porcentajeL > porcentajeB):
-            print("El bebe esta llorando")
+            #print("El bebe esta llorando")
+            emocion = "Llanto"
         else:
-            print("El bebe se encuentra en un estado de bienestar")
+            #print("El bebe se encuentra en un estado de bienestar")
+            emocion = "Bienestar"
+            
+        return emocion
         
     # [END automl_vision_predict]
 
--- main.py
+++ main.py
@@ -1,21 +1,47 @@
 from miband2 import MiBand2
 from apscheduler.schedulers.blocking import BlockingScheduler
 from emocion import predecirEmocion
+from pulsos import reproducirPulso
+from influxdb import InfluxDBClient
+from datetime import datetime
+from mcp3008 import MCP3008
 import threading
 import time
+
+bienestar = True
+ruido = 0
+
+host = 'localhost'
+puerto = 8086
+
+cliente = InfluxDBClient(host, puerto)
+
+try:
+    cliente.switch_database('incubadora')
+    print("Cambiado a BD Incubadora ")
+except:
+    cliente.create_database('incubadora')
+    cliente.switch_database('incubadora')
+    print("Creada BD Incubadora y cambiada")
 
 def obtenerEmocion():
     emocion = predecirEmocion()
     emocion.fotografiar()
-    emocion.predict()
+    emocion = emocion.predict(ruido)
+    global bienestar
+    if 'Llanto' in emocion:
+        bienestar=False
+    else:
+        bienestar=True
+        
     
-def Hilo1():
+def HiloEmociones():
     scheduler = BlockingScheduler()
-    scheduler.add_job(obtenerEmocion, 'interval', minutes=15)
+    scheduler.add_job(obtenerEmocion, 'interval', minutes=2)
     scheduler.start()
 
 
-def Hilo2():
+def HiloCorazon():
     print('Connecting')
     band = MiBand2('FA:01:FB:63:6A:75')
     band.setSecurityLevel(level="medium")
@@ -26,19 +52,57 @@ def Hilo2():
     print("Cont. HRM start")
     band.hrmStopContinuous()
     band.hrmStartContinuous()
+
+    global bienestar
+    musica = reproducirPulso(0)
     while True:
-        band.char_hrm_ctrl.write(b'\x16', True)
-        band.waitForNotifications(1.0)
+        while bienestar:
+            band.char_hrm_ctrl.write(b'\x16', True)
+            band.waitForNotifications(1.0)
+        musica.reproducirMusica()
     
     print("Disconnecting...")
     band.disconnect()
     del band
     
+def obtenerRuido():
+    mcp = MCP3008(0,0)
+    global ruido
+    ruido = mcp.leer(0)
+    print(ruido)
+    if(ruido > 200):
+        obtenerEmocion()
+        
+def HiloRuido():
+    scheduler = BlockingScheduler()
+    scheduler.add_job(obtenerRuido, 'interval', seconds=5)
+    scheduler.start()
+    
+def HiloBD():
+    while True:
+        global ruido
+        global cliente
+        data = [
+            {
+              "measurement": "ruido",
+              "tags": {
+              },
+              "time": datetime.utcnow(),
+              "fields": {
+                "value" : ruido,
+                }
+            }
+        ]
+        cliente.write_points(data)
+    
+    
 if __name__ == "__main__":
-    h=threading.Thread(target=Hilo1)
-    h2=threading.Thread(target=Hilo2)
+    h=threading.Thread(target=HiloEmociones)
+    h2=threading.Thread(target=HiloCorazon)
+    h3=threading.Thread(target=HiloRuido)
+    h4=threading.Thread(target=HiloBD)
     h.start()
     h2.start()
+    h3.start()
+    h4.start()
    
-
-

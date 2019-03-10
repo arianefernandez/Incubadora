6f95328572f1aad8d6bae85390d0db1c1e6bb8d8
Ariane Fernández <ariane.fernandez@opendeusto.es>
2019-03-10 19:03:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ from pulsos import reproducirPulso
 from influxdb import InfluxDBClient
 from datetime import datetime
 from mcp3008 import MCP3008
+import RPi.GPIO as GPIO
 import threading
 import time
 
 bienestar = True
 ruido = 0
 luz = 0
+pin = 18
+duracion = 0
+ocupacion = 0
+ratio = 0
+concentracion = 0
+inicio = 0
+tiempo = 30000
+milis = 0
 
 host = 'localhost'
 puerto = 8086
@@ -24,6 +33,36 @@ except:
     cliente.create_database('incubadora')
     cliente.switch_database('incubadora')
     print("Creada BD Incubadora y cambiada")
+    
+		
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    global pin
+    GPIO.setup(pin, GPIO.IN)
+    
+
+def HiloCalidadAire():
+    setup()
+    while True:
+        global duracion
+        duracion = GPIO.input(pin)
+        global ocupacion
+        ocupacion = ocupacion + duracion
+        
+        global inicio
+        global tiempo
+        global milis
+        milis = int(round(time.time() * 1000))
+        
+        if(milis-inicio > tiempo):	
+            global ratio
+            ratio = ocupacion/(tiempo*10.0)
+            global concentracion
+            concentracion = 1.1*pow(ratio,3)-3.8*pow(ratio,2)+520*ratio+0.62
+            concentracion = int(concentracion)
+            ocupacion = 0
+            inicio = milis
 
 def obtenerEmocion():
     emocion = predecirEmocion()
@@ -35,12 +74,10 @@ def obtenerEmocion():
     else:
         bienestar=True
         
-    
 def HiloEmociones():
     scheduler = BlockingScheduler()
     scheduler.add_job(obtenerEmocion, 'interval', minutes=2)
     scheduler.start()
-
 
 def HiloCorazon():
     print('Connecting')
@@ -72,7 +109,6 @@ def obtenerRuido():
     ruido = mcp.leer(0)
     ruido = (ruido + 40.24)/6.26
     ruido = int(ruido)
-    print(ruido)
     if(ruido > 60):
         obtenerEmocion()
 
@@ -80,21 +116,24 @@ def obtenerLuz():
     mcp = MCP3008(0,0)
     global luz
     luz = mcp.leer(1)
-    print(luz)
+    luz = (luz * 207)/618
+    luz = int(luz)
         
 def HiloRuido():
     scheduler = BlockingScheduler()
-    scheduler.add_job(obtenerRuido, 'interval', seconds=5)
+    scheduler.add_job(obtenerRuido, 'interval', seconds=10)
     scheduler.start()
 
 def HiloLuz():
     scheduler = BlockingScheduler()
-    scheduler.add_job(obtenerLuz, 'interval', seconds=1)
+    scheduler.add_job(obtenerLuz, 'interval', seconds=5)
     scheduler.start()
     
 def HiloBD():
     while True:
         global ruido
+        global luz
+        global concentracion
         global cliente
         data = [
             {
@@ -115,6 +154,15 @@ def HiloBD():
                 "value" : luz,
                 }
             },
+             {
+              "measurement": "calidadAire",
+              "tags": {
+              },
+              "time": datetime.utcnow(),
+              "fields": {
+                "value" : concentracion,
+                }
+            },
             
         ]
         cliente.write_points(data)
@@ -126,8 +174,11 @@ if __name__ == "__main__":
     h3=threading.Thread(target=HiloRuido)
     h4=threading.Thread(target=HiloBD)
     h5=threading.Thread(target=HiloLuz)
+    h6=threading.Thread(target=HiloCalidadAire)
     h.start()
     h2.start()
     h3.start()
     h4.start()
-    h5.start()   
+    h5.start()
+    h6.start()
+    
